51d4445ffc905ccafae15353ef6b25f9429e6b14
Edward A Maxedon Sr <edadma@gmail.com>
2020-04-05 12:52:00 -0400

    make opcodes private

--- src/main/scala/VirtualMachine.scala
+++ src/main/scala/VirtualMachine.scala
@@ -7,6 +7,33 @@ import scala.collection.mutable.ArrayBuffer
 
 object VirtualMachine {
 
+  private object Opcodes {
+    val FETCH: Byte = 0
+    val STORE: Byte = 1
+    val PUSH: Byte  = 2
+    val JMP: Byte   = 3
+    val JZ: Byte    = 4
+    val ADD: Byte   = 5
+    val SUB: Byte   = 6
+    val MUL: Byte   = 7
+    val DIV: Byte   = 8
+    val MOD: Byte   = 9
+    val LT: Byte    = 10
+    val GT: Byte    = 11
+    val LE: Byte    = 12
+    val GE: Byte    = 13
+    val EQ: Byte    = 14
+    val NE: Byte    = 15
+    val AND: Byte   = 16
+    val OR: Byte    = 17
+    val NEG: Byte   = 18
+    val NOT: Byte   = 19
+    val PRTC: Byte  = 20
+    val PRTI: Byte  = 21
+    val PRTS: Byte  = 22
+    val HALT: Byte  = 23
+  }
+
   import Opcodes._
 
   private val HEADER_REGEX = "Datasize: ([0-9]+) Strings: ([0-9]+)" r
@@ -112,7 +139,7 @@ object VirtualMachine {
 
 class VirtualMachine(code: IndexedSeq[Byte], datasize: Int, strings: IndexedSeq[String]) {
 
-  import Opcodes._
+  import VirtualMachine.Opcodes._
 
   var pc      = 0
   val stack   = new mutable.ArrayStack[Int]
@@ -185,34 +212,5 @@ class VirtualMachine(code: IndexedSeq[Byte], datasize: Int, strings: IndexedSeq[
 
     while (running) execute
   }
-
-}
-
-object Opcodes {
-
-  val FETCH: Byte = 0
-  val STORE: Byte = 1
-  val PUSH: Byte  = 2
-  val JMP: Byte   = 3
-  val JZ: Byte    = 4
-  val ADD: Byte   = 5
-  val SUB: Byte   = 6
-  val MUL: Byte   = 7
-  val DIV: Byte   = 8
-  val MOD: Byte   = 9
-  val LT: Byte    = 10
-  val GT: Byte    = 11
-  val LE: Byte    = 12
-  val GE: Byte    = 13
-  val EQ: Byte    = 14
-  val NE: Byte    = 15
-  val AND: Byte   = 16
-  val OR: Byte    = 17
-  val NEG: Byte   = 18
-  val NOT: Byte   = 19
-  val PRTC: Byte  = 20
-  val PRTI: Byte  = 21
-  val PRTS: Byte  = 22
-  val HALT: Byte  = 23
 
 }
